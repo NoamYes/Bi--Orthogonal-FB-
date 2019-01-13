b23b736904c17ba547f412d4814902952f26dda8
NoamProg <38458454+NoamProg@users.noreply.github.com>
2019-01-13 13:08:39 +0200

    stft

--- Q4.m
+++ Q4.m
@@ -10,8 +10,25 @@ f = pi/2;    % Cutoff frequencies
 a = [1 0];        % Desired amplitudes
 fs = pi;
 
-dev = [(10^(rp/20)-1)/(10^(rp/20)+1)  10^(-rs/20)]; 
-[n,fo,ao,w] = firpmord(f,a,dev, fs);
+[n,fo,ao,w] = firpmord([f-0.01*f f],[1 0],[0.01 0.01],fs);
 b = firpm(n,fo,ao,w);
 freqz(b,1,1024,fs)
 title('Lowpass Filter Designed to Specifications')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% Question 5 - STFT
+
